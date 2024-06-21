d30d9cc66b9e1513b4a960b15954a2caf2d3e72f
Irene Gu <gu.jiawen@northeastern.edu>
2024-06-21 14:58:48 -0700

    chore(controller):add route

--- realEstate/controllers/controllers.py
+++ realEstate/controllers/controllers.py
@@ -3,7 +3,7 @@ from odoo.http import request
 from odoo.http import route
 
 
-# class RealEstateController(http.Controller):
-#     @http.route('/RealEstate',type='http', auth='public',website=True)
-#     def index(self,**kwargs):
-#         return "RealEstate"
+class RealEstateController(http.Controller):
+    @http.route('/RealEstate',type='http', auth='public',website=True)
+    def index(self,**kwargs):
+        return "RealEstate"
